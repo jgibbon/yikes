7ff7f30660c36476f54c441da5fce48b240c6c9b
Nokius <mail@nokius.net>
2018-07-17 00:21:25 +0200

    [python] to tome get rest asap

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
 import os, re, sys, time, socket
 srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 srv.connect(("192.168.42.1", 7878))
-
 get_token = '{"msg_id":257,"token":0}'
+srv.send(get_token)
+msg_with_token = srv.recv(512)
+token = re.findall('"param":(.+?),',msg_with_token)[0]
 get_battery = '{"msg_id":13,"token":%s}' %token
 get_settings = '{"msg_id":3,"token":%s}' %token
 sdcard_space ='{"msg_id":5,"type":"free","token":%s}' %token
@@ -15,5 +17,6 @@ get_recording_time = '{"msg_id":515,token":%s}' %token
 take_picture = '{"msg_id":769,token":%s}' %token
 
 
-srv.send()
+srv.send(take_picture)
 srv.recv(512) #max needs increae 
+
